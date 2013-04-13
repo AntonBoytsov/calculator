a937427d1ed4b79c077c748a5f40eb82e1f27aed
daklyuev <daklyuev@804a-09.comp.susu.ac.ru>
2013-04-13 13:04:32 +0600

    Страница калькулятора

--- CalcApp/app/controllers/Calculator.java
+++ CalcApp/app/controllers/Calculator.java
@@ -6,7 +6,7 @@ import play.mvc.Result;
 public class Calculator extends Controller {
 	
 	public static Result index() {
-		return ok("test");
+		return ok(views.html.calculator.render());
 	}
 	
 }
